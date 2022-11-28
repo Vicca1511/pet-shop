92937bcfba930231692e76710020e935b0bfa2ac
Lucas Viccari <lviccari4@gmail.com>
2022-11-28 18:05:37 -0300

    feat:  finished Controller

--- src/owner/owner.controller.ts
+++ src/owner/owner.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { Console } from 'console';
 import { IOwnerEntity } from './entities/owner.entity';
 import { ownerDto } from './entities/services/dto/ownerImput.dto';
@@ -13,6 +21,16 @@ export class ownerController {
   async getAllOwners(): Promise<IOwnerEntity[]> {
     return this.service.getAllOwners();
   }
+
+  @Get(':id')
+  async getOwnerById(@Param('id') userId: string): Promise<IOwnerEntity> {
+    try {
+      return await this.service.getOwnerById(userId);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   @Post()
   async createOwner(
     @Body() { name, cpf, email, petEspecies, petName, role }: ownerDto,
@@ -34,6 +52,20 @@ export class ownerController {
   async updateOwner(@Body() ownerData: partialOwnerDto): Promise<IOwnerEntity> {
     try {
       return await this.service.updateOwner(ownerData);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  @Delete(':id')
+  async deleteOwner(@Param() ownerId: string): Promise<String> {
+    try {
+      const ownerIsDeleted = await this.service.deleteOwnerById(ownerId);
+      if (ownerIsDeleted) {
+        return 'Owner deleted successfully';
+      } else {
+        return 'Owner not found';
+      }
     } catch (err) {
       console.log(err);
     }
