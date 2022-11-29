5fc6a9e7c023efac14b1c11209415544f8746377
Lucas Viccari <lviccari4@gmail.com>
2022-11-28 21:20:31 -0300

    feat: conected db and swagger documantation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ownerService } from './owner/entities/services/owner.service';
 import { ownerController } from './owner/owner.controller';
+import { OwnerRepository } from './owner/owner.repository';
+import { DatabaseModule } from './prisma/dataBase.module';
 
 
 @Module({
+    imports: [DatabaseModule],
     controllers:[ownerController],
-    providers:[ownerService]
+    providers:[ownerService , OwnerRepository]
 })
 export class AppModule {}
